8405880dd9e30852921f594500be1c0751ee7883
abcdefopquan <nguyenquan14124@gmail.com>
2023-12-10 19:12:29 +0700

    abc

---
+++ DAL/Repos/ThongKeRepo.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Context;
+using DAL.DomainClass;
+using DAL.IRepos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class ThongKeRepo : IThongKe
+    {
+        public List<ThongKe> GetAll()
+        {
+            using (var Context = new DBContext())
+            {
+                return Context.ThongKes.ToList();
+            }
+        }
+    }
+}
